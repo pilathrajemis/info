81b709fd7604ea4fc734f00c95dde685dff26227
pilathraj <pilathraj@gmail.com>
2020-08-14 10:29:28 +0530

    Create index.js

---
+++ React/tutorial/hello-world-arrow-function/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//Functional component with arrow function
+const App = ()=> <h1>Hello world!</h1>
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
