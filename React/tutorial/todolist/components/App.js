28805f7058fb1b3d75462fa11588b39e252b3c45
pilathraj <pilathraj@gmail.com>
2020-08-14 10:22:34 +0530

    Add files via upload

---
+++ React/tutorial/todolist/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App(){
+  return (
+      <div>
+        <h1> Todo list </h1>
+        <ul>
+        <li><input type="checkbox"/> item1</li>
+        <li><input type="checkbox"/> item2</li>
+        <li><input type="checkbox"/> item3</li>
+        <li><input type="checkbox"/> item4</li>
+        </ul>
+      </div>
+  )  
+}
+export default App
