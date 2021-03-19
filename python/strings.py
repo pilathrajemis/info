631a30401494b1cc62f8fa275b69bc588960c061
pilathraj <pilathraj@gmail.com>
2021-03-19 17:59:30 +0530

    Update strings.py

--- python/strings.py
+++ python/strings.py
@@ -1,1 +1,8 @@
-
+# Strings
+## index Slice
+n = "0123456789"
+print(n) #0123456789
+print(n[1]) # 1  -> index start from Zero
+print(n[1:3]) # 12  -> Start and end-1 index, Print from 1 to 2 index 
+print(n[6:]) # 6789
+print(n[:5]) # 01234
